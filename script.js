84bf11acb651191a60743334c20324fd6a79e2c2
practise <rezwanamimi11@gmail.com>
2017-10-10 22:06:23 +0600

    create html using jquery and script file

---
+++ script.js
@@ -0,0 +1,18 @@
+//var hello = (h1);
+//var Heading  = hello.html;
+//console.log(Heading);
+
+//var Heading = jQuery('h1')
+
+
+var Heading = $ ('h1');
+var paragraph = $ ('#first_paragraph');
+paragraph.hide(4000);
+
+Heading.on('mouseover', function (){
+
+
+alert ('hiii');
+});
+
+
